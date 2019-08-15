0403c160b0d0c8342e54fefe97581f6550f601fb
Liam Ryan <liomry@gmail.com>
2019-08-16 00:45:24 +0100

    Working movement, camera needs some more work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,34 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[RequireComponent(typeof(Rigidbody2D))]
+﻿using UnityEngine;
+
 public class PlayerController : MonoBehaviour {
-    public float thrust;
+	public float thrust;
+	public float maxVelocity;
+	private Vector3 velocity;
 
-    public float rotationSpeed;
-    private Vector2 move;
-    private Rigidbody2D body;
-    
-    // Start is called before the first frame update
-    private void Start() {
-        body = gameObject.GetComponent<Rigidbody2D>();
-    }
+	public float rotationSpeed;
+	private Vector3 move;
 
-    // Update is called once per frame
-    private void Update() {
-        var verticalInput = Input.GetAxis("Vertical");
-        move.y = verticalInput >= 0 ? verticalInput : 0;
-        move.x = Input.GetAxis("Horizontal");
-        if (move.x != 0) {
-            Debug.LogFormat("Rotation is {0}", body.rotation);
-            body.MoveRotation(body.rotation - (rotationSpeed * move.x * 100 * Time.deltaTime));
-            Debug.LogWarningFormat("Rotation after adjustment is {0}", body.rotation);
-        }
+	private void Start() {
+		velocity = new Vector2(0, 0);
+	}
+	
+	private void Update() {
+		var verticalInput = Input.GetAxis("Vertical");
+		move.y = verticalInput >= 0 ? verticalInput : 0;
+		move.x = Input.GetAxis("Horizontal");
+		
+		var rotationThisUpdate = -(rotationSpeed * move.x * 100 * Time.deltaTime);
+		if (Mathf.Abs(move.x) > Mathf.Epsilon) {
+			transform.Rotate(0f, 0f, rotationThisUpdate);
+		}
 
-//        body.MovePosition();
-        /*if (verticalInput > 0) 
-            Debug.LogFormat("verticalInput is {0}", verticalInput);
-        if(Mathf.Abs(move.x) > Mathf.Epsilon)
-            Debug.LogFormat("horizontalInput is {0}", move.x);*/
-    }
+		if (move.y > 0) {
+			velocity += move.y * Time.deltaTime * thrust * transform.up;
+			velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
+		}
+	}
+
+	private void FixedUpdate() {
+		transform.position += velocity;
+	}
 }
