0ff18c71da667fe2946573e694c8e200985e65f7
Liam Ryan <liomry@gmail.com>
2019-08-17 16:51:58 +0100

    Basic mechanics implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float thrust;
 	public float maxVelocity;
+	public float gravity;
+	public float maxCollisionVelocity;
+	
 	private Vector3 velocity;
+	private readonly Vector3 gravityVector = new Vector3(0, -0.1f, 0);
 
 	public float rotationSpeed;
 	private Vector3 move;
@@ -24,11 +29,45 @@ public class PlayerController : MonoBehaviour {
 
 		if (move.y > 0) {
 			velocity += move.y * Time.deltaTime * thrust * transform.up;
-			velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
 		}
+
+		velocity += Time.deltaTime * gravity * gravityVector;
+		velocity = Vector3.ClampMagnitude(velocity, maxVelocity);
 	}
 
 	private void FixedUpdate() {
 		transform.position += velocity;
 	}
+
+	private void Die() {
+		Debug.Log("Dead!");
+	}
+
+	private void LevelComplete() {
+		Debug.Log("Victory!");
+	}
+
+	private bool IsTooFast() {
+		Debug.LogFormat("Velocity magnitude is {0}", velocity.magnitude);
+		return velocity.magnitude > maxCollisionVelocity;
+	}
+
+	private void BounceOffGeometry(Collision2D other) {
+		Debug.Log("Bounce");
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		Debug.Log("Collision!");
+		Debug.LogFormat("Collided with {0}", other.gameObject.name);
+		if (IsTooFast()) {
+			Die();
+		} else {
+			if (other.gameObject.CompareTag("Finish")) {
+				LevelComplete();
+			} else {
+				BounceOffGeometry(other);
+			}
+		}
+	}
+	
 }
